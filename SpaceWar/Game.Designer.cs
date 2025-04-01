e70eee76135e7f9a426e56e8fcd986c39f634896
MohammadHosein <mohammadhamze1387@gmail.com>
2025-04-01 17:00:20 +0330

    Add project files.

---
+++ SpaceWar/Game.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SpaceWar
+{
+    partial class Game
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.fighterplane;
+            pictureBox1.Location = new Point(935, 600);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(132, 136);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Game
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 255);
+            ClientSize = new Size(1354, 748);
+            Controls.Add(pictureBox1);
+            Name = "Game";
+            Text = "Space War Game";
+            Load += this.Game_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+    }
+}
---
+++ SpaceWar/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceWar
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+            pictureBox1.Top = (this.ClientSize.Height) - 145;
+
+            pictureBox1.Anchor = AnchorStyles.None;
+        }
+    }
+}
